c581121f51e74e8f2df0053985e1971a1214827d
Shraddha-Kokare <139088829+Shraddha-Kokare@users.noreply.github.com>
2025-07-29 23:11:49 +0530

    Update blogModel.js

--- backend/models/blogModel.js
+++ backend/models/blogModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://kokareshraddha5:kokareshraddha5@cluster0.syk17gx.mongodb.net/Blog?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect("MONGO DB  URI")
   .then(() => console.log("MongoDB connected"))
   .catch(err => {
     console.error("MongoDB connection error:", err.message);
